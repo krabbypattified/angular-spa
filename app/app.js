1c6c0d7578bb22751af2f6c3a8b0ca68ed2d9279
Gabe Rogan <rogangabe@gmail.com>
2016-11-15 15:57:47 -0500

    Angular Works

--- app/app.js
+++ app/app.js
@@ -1,8 +1,9 @@
 var app = angular.module('app', ['ui.materialize'])
 
-.controller('globalCtrl', ["$scope", function ($scope) {
-    $scope.select = {
-        value: "Option1",
-        choices: ["Option1", "I'm an option", "This is materialize", "No, this is Patrick."]
-    };
+.controller('GlobalCtrl', ["$scope", function ($scope) {
+  $scope.title = "News";
+  $scope.select = {
+      value: "Option1",
+      choices: ["Option1", "I'm an option", "This is materialize", "No, this is Patrick."]
+  };
 }]);
